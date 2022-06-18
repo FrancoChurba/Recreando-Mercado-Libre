0e8f5fda8fbccb51da300f18afe9e092c1dc5044
FrancoChurba <churbafranco@gmail.com>
2022-06-18 16:12:35 -0300

    agregando css

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-app.use(express.static('/public'));
+const publicPath = path.resolve(__dirname, './public');
 
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
+app.use(express.static(publicPath));
 
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/home.html')));
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
+
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, './views/login.html')));
+
+app.get('/registro', (req, res) => res.sendFile(path.join(__dirname, './views/register.html')));
 
 app.listen(process.env.PORT || 3000,function(){
     console.log('Servidor funcionando');
